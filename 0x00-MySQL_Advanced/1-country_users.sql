8aab0f1c3286d616b6137f89fde935e371ca9909
KwameSei <nathanielosei92@gmail.com>
2023-04-07 04:47:00 +0000

    Creating users table with enum column

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,12 @@
 -- creating a table of users consisting of the following columns:
 -- id, integer, never null
+-- country, enumeration of countries: US, CO and TN, never null (default = US)
 -- email, string (255)
 -- name, string (255)
 
 CREATE TABLE IF NOT EXISTS users (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255)
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );

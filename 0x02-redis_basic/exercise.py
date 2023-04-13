b633cb367ea3e75c49c575bd3d20990d14497c89
KwameSei <nathanielosei92@gmail.com>
2023-04-13 04:49:46 +0000

    Writing and reading from  redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,9 +15,33 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Method tht stores data """
+        """ Method the stores data """
         # Generating a random key
         key = str(uuid4())
         # Storing the input data in Redis using the random key
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None) -> \
+            Union[str, bytes, int, float]:
+        """ Method that gets data in any format """
+        # Retrieving the data stored in Redis using the key
+        data = self._redis.get(key)
+        # Apply fn to the data if it exists
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Method that gets data as a string """
+        # Retrieving the data stored in Redis using the key
+        data = self._redis.get(key)
+        # Converting the data to a string
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """ Method that gets data as an integer """
+        # Retrieving the data stored in Redis using the key
+        data = self._redis.get(key)
+        # Converting the data to an integer
+        return int.from_bytes(data, 'big')
